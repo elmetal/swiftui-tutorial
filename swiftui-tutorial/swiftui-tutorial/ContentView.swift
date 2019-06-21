92d23a141b411497517c3909fbdaac0f69195434
elmetal <nm7.pupu@gmail.com>
2019-06-22 00:15:46 +0900

    init tutorial

---
+++ swiftui-tutorial/swiftui-tutorial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-tutorial
+//
+//  Created by 榎原聖太 on 2019/06/22.
+//  Copyright © 2019 elmetal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
