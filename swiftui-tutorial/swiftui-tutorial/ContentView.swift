764e156511aae7057b7147a05b820b1256f04a82
elmetal <nm7.pupu@gmail.com>
2019-06-22 00:16:13 +0900

    wip section2

--- swiftui-tutorial/swiftui-tutorial/ContentView.swift
+++ swiftui-tutorial/swiftui-tutorial/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+        Text("Hello World").font(.title).color(.orange)
+        Text("All over the world")
+        }
     }
 }
 
